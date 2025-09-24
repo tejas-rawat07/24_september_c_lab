10c271277374ffdf1b7497e1da13f330e6365c40
tejas-rawat07 <tejassrawat07@gmail.com>
2025-09-24 11:43:09 +0530

    Update sep_24_2.c

--- sep_24_2.c
+++ sep_24_2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 int main() {
     char str[200];
@@ -26,4 +25,5 @@ int main() {
     printf("special characters- %d\n", special);
 
     return 0;
+
 }
